e01ea39199a1e0dce92cebe0038caf77609212ef
paolochmprojects <paolo.dev.projects@gmail.com>
2024-08-10 18:57:15 -0500

    [ADD] new enpoint for permissions roles

--- src/permission/dto/update-permission.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { AddPermissionDto } from './add-permission.dto';
-
-export class UpdatePermissionDto extends PartialType(AddPermissionDto) {}
